8d063743de29e96f458b4827e8a4f7db1a8c6b3c
akkiiii11 <137610282+akkiiii11@users.noreply.github.com>
2024-04-04 12:10:32 +0530

    Add files via upload

--- MethodOverridingDemo.java
+++ MethodOverridingDemo.java
@@ -1,26 +1,10 @@
 package com.thread;
 
-//@FunctionalInterface
-//interface Runnable
-//{
-//	public void run();
-//}
-//
-//class Thread implements Runnable
-//{
-//
-//	@Override
-//	public void run() {
-//		// TODO Auto-generated method stub
-//		
-//	}
-//	
-//}
-
-//class MyThread3 extends Thread
-//{
-//	
-//}
+interface Employee
+{
+	public void salary();
+	
+}
 
 public class MethodOverridingDemo {
 
