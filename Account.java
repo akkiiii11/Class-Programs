b7e47c38b3ef91a658c8d01dd254fc839ec33b54
akkiiii11 <137610282+akkiiii11@users.noreply.github.com>
2024-04-04 11:53:47 +0530

    Add files via upload

--- Account.java
+++ Account.java
@@ -1,11 +1,31 @@
-package InterfaceBank;
+package com.thread;
 
-public interface Account {
+public class Account extends Thread
+{
+	static int balance = 1000;
 	
-	void deposit(double amount);
+	public void run()
+	{
+		if(balance>800)
+		{
+			System.out.println(Thread.currentThread().getName() + " Your balance is " + balance + " " + "rs you can withdraw the amount");
+			
+			try 
+			{
+				sleep(1000);
+			} 
+			catch (InterruptedException e) 
+			{
+				e.printStackTrace();
+			}
+			balance=200;
+			System.out.println(Thread.currentThread().getName() + " After withdrawal your balnce is " + "balance is " +balance );
+		}
+		else
+		{
+			System.out.println(Thread.currentThread().getName() + " Your balance is " + balance + "so you cant withdraw the amount");
+		}
+		
+	}
 	
-	void withdraw(double amount);
-	
-	double getBalance();
-
 }
---
+++ MethodOverridingDemo.java
@@ -0,0 +1,33 @@
+package com.thread;
+
+//@FunctionalInterface
+//interface Runnable
+//{
+//	public void run();
+//}
+//
+//class Thread implements Runnable
+//{
+//
+//	@Override
+//	public void run() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//	
+//}
+
+//class MyThread3 extends Thread
+//{
+//	
+//}
+
+public class MethodOverridingDemo {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
